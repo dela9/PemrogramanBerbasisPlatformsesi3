f4d75b7de175daa1ba6496bd0b08915c50d2d4a6
dela9 <124057172+dela9@users.noreply.github.com>
2023-10-20 20:29:53 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,24 @@
+let restaurantObject = {
+   Name: "Cafe Carla",
+   City: "Lampung",
+   Favorite_drink: "Mocha",
+   Favorite_food: "Salad",
+   isVegan: false
+};
+
+let restaurantArray = [
+{
+   Name: "Cafe carla",
+   City: "Lampung",
+   Favorite_drink: "Mocha",
+   Favorite_food: "Salad",
+   isVegan: false
+},
+{
+   Name: "Del’s Cafe",
+   City: "Sukabumi",
+   Favorite_drink: "Macha",
+   Favorite_food: "Cheese Cake",
+   isVegan: true
+}
+];
